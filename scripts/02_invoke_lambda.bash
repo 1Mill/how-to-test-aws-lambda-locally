8a9f5a92c6e5f453b0f17f4f4e945fe3ddedbcf9
1Mill <erik.ekberg@outlook.com>
2023-04-10 16:06:55 -0700

    Invoke with payload

--- scripts/02_invoke_lambda.bash
+++ scripts/02_invoke_lambda.bash
@@ -9,13 +9,16 @@ function invoke_lambda() {
 	local function_name=hello-world-v0
 
 	# * Invoke configuration
+	local invoke_payload='{ "name": "Alice" }'
 	local invoke_to_file=response.json
 	local invoke_type=RequestResponse
 
 	# * Invoke lambda from LocalStack
 	aws --endpoint $endpoint lambda invoke \
+		--cli-binary-format raw-in-base64-out \
 		--function-name $function_name \
 		--invocation-type $invoke_type \
+		--payload "$invoke_payload" \
 		$invoke_to_file
 }
 
