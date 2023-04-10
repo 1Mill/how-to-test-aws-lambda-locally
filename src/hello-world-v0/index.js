236d2f482ac2aaa389b0656d5cb29e52833f6e3b
1Mill <erik.ekberg@outlook.com>
2023-04-10 16:06:23 -0700

    Return payload

--- src/hello-world-v0/index.js
+++ src/hello-world-v0/index.js
@@ -1,10 +1,4 @@
-exports.handler = (event = {}, ctx = {}) => {
-	console.log("--------------------------------------------------------")
-	console.log("STARTING -----------------------------------------------")
-	console.log(JSON.stringify(ctx, null, 4))
-	console.log(JSON.stringify(event, null, 4))
-	console.log("ENDING -------------------------------------------------")
-	console.log("--------------------------------------------------------")
-
-	return 'Hello world!'
+exports.handler = async (event = {}, ctx = {}) => {
+	console.log({ event })
+	return { event }
 }
