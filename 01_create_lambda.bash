d10d3450da019f525101cc5ddc67d7bf9e7c52d6
1Mill <erik.ekberg@outlook.com>
2023-04-10 20:43:01 -0700

    Remove delete behavior from creat

--- 01_create_lambda.bash
+++ 01_create_lambda.bash
@@ -15,8 +15,6 @@ function create_lambda() {
 	local function_role=arn:aws:iam::000000000000:role/localstack-does-not-care
 	local function_runtime=nodejs18.x
 
-	aws --endpoint-url $endpoint lambda delete-function --function-name $function_name || true
-
 	# * Create lambda in LocalStack
 	aws --endpoint-url $endpoint lambda create-function \
 		--code S3Bucket="hot-reload",S3Key="$dir" \
